74f81b26259b817ee9eaefab919ee5cd634553fb
Dominik Palla <dominik.palla@uhk.cz>
2024-05-11 15:41:43 +0200

    Initial commit

---
+++ src/main/java/cz/uhk/ppro/PproApplication.java
@@ -0,0 +1,13 @@
+package cz.uhk.ppro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PproApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/uhk/ppro/PproApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.uhk.ppro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PproApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
