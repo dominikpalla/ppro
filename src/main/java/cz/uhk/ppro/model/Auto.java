ac230cde29ed5aac5a7a6103cc855e4d406b8421
Dominik Palla <dominik.palla@uhk.cz>
2024-05-11 15:44:30 +0200

    Auto model class

---
+++ src/main/java/cz/uhk/ppro/model/Auto.java
@@ -0,0 +1,50 @@
+package cz.uhk.ppro.model;
+
+public class Auto {
+
+    private Long id;
+    private String spz;
+    private String color;
+    private float tankVolume;
+    private int numberOfSeats;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSpz() {
+        return spz;
+    }
+
+    public void setSpz(String spz) {
+        this.spz = spz;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public float getTankVolume() {
+        return tankVolume;
+    }
+
+    public void setTankVolume(float tankVolume) {
+        this.tankVolume = tankVolume;
+    }
+
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+
+    public void setNumberOfSeats(int numberOfSeats) {
+        this.numberOfSeats = numberOfSeats;
+    }
+}
