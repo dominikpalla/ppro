f02f75bd010bb0bb3ef7e572f51fc57620391ff9
Dominik Palla <dominik.palla@uhk.cz>
2024-05-11 15:49:44 +0200

    Repair of controller

--- src/main/java/cz/uhk/ppro/controller/HelloController.java
+++ src/main/java/cz/uhk/ppro/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("")
+    @GetMapping("/")
     public String hello(){
         return "hello";
     }
